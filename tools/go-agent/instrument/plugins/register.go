51f6130905ad88ed5d515aef5d2db2790d23a62d
Jiekun <jiekun.dev@gmail.com>
2023-06-08 12:13:04 +0800

    [feature: go-redis] Update codes with goimports

--- tools/go-agent/instrument/plugins/register.go
+++ tools/go-agent/instrument/plugins/register.go
@@ -21,7 +21,7 @@ import (
 	"github.com/apache/skywalking-go/plugins/core/instrument"
 	"github.com/apache/skywalking-go/plugins/dubbo"
 	"github.com/apache/skywalking-go/plugins/gin"
-	"github.com/apache/skywalking-go/plugins/go-redisv9"
+	goredisv9 "github.com/apache/skywalking-go/plugins/go-redisv9"
 	"github.com/apache/skywalking-go/plugins/go-restfulv3"
 	gorm_entry "github.com/apache/skywalking-go/plugins/gorm/entry"
 	gorm_mysql "github.com/apache/skywalking-go/plugins/gorm/mysql"
