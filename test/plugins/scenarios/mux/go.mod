c890aa99b1d650afa71ad8f3b10ddc94e287655b
fzdwx <likelovec@gmail.com>
2024-07-12 23:06:55 +0800

    feat: add test case

--- test/plugins/scenarios/mux/go.mod
+++ test/plugins/scenarios/mux/go.mod
@@ -2,4 +2,7 @@ module test/plugins/scenarios/mux
 
 go 1.18
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
+)
--- test/plugins/scenarios/mux/main.go
+++ test/plugins/scenarios/mux/main.go
@@ -18,9 +18,11 @@
 package main
 
 import (
+	"github.com/gorilla/websocket"
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"time"
 
 	_ "github.com/apache/skywalking-go"
@@ -54,12 +56,50 @@ func health(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("success"))
 }
 
+var upgrader = websocket.Upgrader{}
+
+func ws(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	defer c.Close()
+	for {
+		mt, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("recv: %s", message)
+		err = c.WriteMessage(mt, message)
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.Path("/health").HandlerFunc(health)
 	r.Path("/consumer").HandlerFunc(consumer)
 	r.PathPrefix("/provider").Path("/{var}").HandlerFunc(provider)
+	r.Path("/ws").HandlerFunc(ws)
 
+	go func() {
+		time.Sleep(3 * time.Second)
+
+		u := url.URL{Scheme: "ws", Host: "localhost:8080", Path: "/ws"}
+		log.Printf("connecting to %s", u.String())
+		c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+		if err != nil {
+			log.Fatal("dial:", err)
+		}
+		defer c.Close()
+		c.WriteMessage(websocket.TextMessage, []byte("hello from mux test"))
+		c.Close()
+	}()
 	// Bind to a port and pass our router in
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
