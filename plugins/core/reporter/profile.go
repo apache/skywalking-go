ac8175a0ee58a8af813224a87600e270f1b5cd01
cqhasy <3181940782@qq.com>
2025-07-30 22:43:00 +0800

    fix(add some profile tools)

--- plugins/core/reporter/profile.go
+++ plugins/core/reporter/profile.go
@@ -2,6 +2,7 @@ package reporter
 
 import (
 	"bytes"
+	"context"
 	"fmt"
 	"github.com/pkg/errors"
 	"runtime/pprof"
@@ -12,8 +13,6 @@ import (
 )
 
 type TaskStatus int
-
-var cpuProfileLock sync.Mutex
 
 const (
 	Pending TaskStatus = iota
@@ -35,13 +34,18 @@ type Task struct {
 	Status               TaskStatus //任务执行状态
 }
 type ProfileManager struct {
-	mu    sync.Mutex
-	Tasks map[string]*Task
+	mu       sync.Mutex
+	ctx      context.Context
+	status   bool
+	Tasks    map[string]*Task
+	buf      *bytes.Buffer // 当前 profile 的 buffer
+	stopChan chan struct{} //结束信号
 }
 
 func NewProfileManager() *ProfileManager {
 	return &ProfileManager{
-		Tasks: make(map[string]*Task),
+		Tasks:  make(map[string]*Task),
+		status: false,
 	}
 }
 func (m *ProfileManager) AddProfileTask(args []*common.KeyStringValuePair) {
@@ -73,7 +77,7 @@ func (m *ProfileManager) AddProfileTask(args []*common.KeyStringValuePair) {
 	}
 	task.Status = Pending
 	fmt.Println("adding task:", task)
-	m.Tasks[task.SerialNumber] = &task
+	m.Tasks[task.TaskId] = &task
 }
 func (m *ProfileManager) RemoveProfileTask() {
 	m.mu.Lock()
@@ -95,49 +99,114 @@ func (m *ProfileManager) GetProfileTask(endpoint string) []*Task {
 	}
 	return result
 }
-func (m *ProfileManager) ToProfile(endpoint string, traceId string) {
+func (m *ProfileManager) IfProfiling() bool {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	return m.status
+}
+
+func (m *ProfileManager) ToProfile(endpoint string, traceSegmentID string) {
 	t := time.Now().UnixMilli()
-	//fmt.Println(traceId + "  " + endpoint + "  " + "start")
+
 	for _, v := range m.GetProfileTask(endpoint) {
 		//删除过期任务
 		if v.StartTime+int64(v.Duration)*int64(time.Minute) < t {
 			delete(m.Tasks, v.SerialNumber)
 			continue
 		}
-		v.Status = Running
+		m.Tasks[v.TaskId].Status = Running
 		//执行profiling
 		task := v
 		go func(task *Task) {
-			data, err := StartProfiling(task)
+			err := m.StartProfiling(task, traceSegmentID)
 			if err != nil {
-				task.Status = Pending
+				m.Tasks[task.TaskId].Status = Pending
 				return
 			}
-			fmt.Println(data)
+			m.Tasks[task.TaskId].Status = Finished
 		}(task)
 
 	}
 }
-func StartProfiling(t *Task) ([]byte, error) {
-	var buf bytes.Buffer
-	if cpuProfileLock.TryLock() {
-		defer cpuProfileLock.Unlock()
-		// 开始写入 profiling 数据到内存
-		if err := pprof.StartCPUProfile(&buf); err != nil {
-			fmt.Printf("could not start CPU profile: %v\n", err)
-			return nil, err
-		}
+func (m *ProfileManager) StartProfiling(t *Task, traceSegmentID string) error {
+	m.mu.Lock()
+	if m.status {
+		m.mu.Unlock()
+		return errors.New("profile is already running")
+	}
+	m.status = true
+	m.buf = &bytes.Buffer{}
+	m.stopChan = make(chan struct{})
+	m.mu.Unlock()
+	//添加profiling主上下文
+	ctx := context.Background()
+	labels := pprof.Labels("traceSegmentID", traceSegmentID)
+	ctx = pprof.WithLabels(ctx, labels)
+	pprof.SetGoroutineLabels(ctx)
+	m.mu.Lock()
+	m.ctx = ctx
+	m.mu.Unlock()
 
-		// 模拟运行（持续 t.Duration min）
-		time.Sleep(time.Duration(t.Duration) * time.Minute)
-		pprof.StopCPUProfile()
-		return buf.Bytes(), nil
-		// profiling 逻辑
-	} else {
-		//fmt.Println("profile is already running, skip this task")
-		return nil, errors.New("profile is already running")
+	if err := pprof.StartCPUProfile(m.buf); err != nil {
+		m.mu.Lock()
+		m.status = false
+		m.mu.Unlock()
+		return err
 	}
 
+	select {
+	// 超时结束
+	case <-time.After(time.Duration(t.Duration) * time.Minute):
+
+	case <-m.stopChan: // 手动结束
+	}
+	// 停止
+	pprof.StopCPUProfile()
+	m.mu.Lock()
+	m.status = false
+	close(m.stopChan)
+	m.stopChan = nil
+	m.mu.Unlock()
+	return nil
+}
+func (m *ProfileManager) AddSpanId(spanID string) {
+	if m.ctx == nil {
+		return
+	}
+
+	spanCtx := pprof.WithLabels(m.ctx, pprof.Labels("spanID", spanID))
+	pprof.SetGoroutineLabels(spanCtx)
+}
+func (m *ProfileManager) EndProfiling() {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	//如果是false,即没启动，就直接返回
+	if !m.status {
+		return
+	}
+
+	if m.stopChan != nil {
+		// 通知 goroutine 停止
+		close(m.stopChan)
+	}
+}
+
+func (m *ProfileManager) ReportResult() ([]byte, error) {
+	// goroutine 会调用 StopCPUProfile 并清理状态
+	// 但 StopCPUProfile 不是完全同步的，最好等待 status=false
+	for m.status {
+		m.mu.Unlock()
+		time.Sleep(10 * time.Millisecond)
+		m.mu.Lock()
+	}
+	if m.buf == nil {
+		return nil, errors.New("no buffer")
+	}
+
+	data := m.buf.Bytes()
+	m.buf = nil
+	m.ctx = nil
+	return data, nil
 }
 func splitProfileData(data []byte, chunkSize int) [][]byte {
 	var chunks [][]byte
